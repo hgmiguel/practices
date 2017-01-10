198900052b6d3ba4a9d1639f6a83fb72f69dc376
miguel.huerta <miguel.huerta@globant.com>
2017-01-10 11:39:23 -0600

    Se agrega plantilla para crear el stack con cloudformation

--- wooecommerce/aws-commands.sh
+++ wooecommerce/aws-commands.sh
@@ -1,4 +1,4 @@
-
+# Create an application
 aws deploy create-application \
   --application-name RegalosYDetallesDiamante \
   --profile=hgmiguel
@@ -16,11 +16,30 @@ aws deploy create-deployment-group \
   --ec2-tag-filters Key=Name,Value=RegalosYDetallesDiamante,Type=KEY_AND_VALUE \
   --service-role-arn arn:aws:iam::408460586533:role/RegalosYDetallesDiamante-CodeDeployTrustRole-19F0YAFFJIRUM \
   --profile=hgmiguel
+#####
 
+
+## create cloudformation
+aws cloudformation create-stack \
+    --stack-name RegalosYDetallesDiamante \
+    --template-body file:///home/miguel.huerta/Source/practices/wooecommerce/CodeDeploy.yml \
+    --parameters ParameterKey=InstanceCount,ParameterValue=1 ParameterKey=InstanceType,ParameterValue=t1.micro \
+      ParameterKey=KeyPairName,ParameterValue=public-globant-hgmiguel-key ParameterKey=OperatingSystem,ParameterValue=Linux \
+      ParameterKey=SSHLocation,ParameterValue=0.0.0.0/0 ParameterKey=TagKey,ParameterValue=Name \
+      ParameterKey=TagValue,ParameterValue=regalosydetallesdiamante \
+    --capabilities CAPABILITY_IAM \
+    --profile hgmiguel
+
+
+####
+
+# Deploy and redeploy
 aws deploy create-deployment \
   --application-name RegalosYDetallesDiamante \
   --deployment-config-name CodeDeployDefault.OneAtATime \
   --deployment-group-name RegalosYDetallesDiamante_DepGroup \
   --s3-location bucket=hgmiguel.regalosydetallesdiamante,bundleType=zip,key=RegalosYDetallesDiamante.zip \
   --profile=hgmiguel
+
+
 
